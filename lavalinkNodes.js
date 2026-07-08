29e3362325cc05e059249880c6482ad198066b49
royalngr2216 <royalngr2216@gmail.com>
2026-07-09 01:04:39 +0530

    Update lavalinkNodes.js

--- lavalinkNodes.js
+++ lavalinkNodes.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     name: "main-node",
-    host: "https://lavalink-wzmz.onrender.com",
+    host: "lavalink-wzmz.onrender.com",
     port: 443,
     password: "nagar123",
     secure: true,
